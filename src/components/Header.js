2a1844b66d7221155e9f2db4965cf9bc46c7606d
RabbltMan <82202653+RabbltMan@users.noreply.github.com>
2023-02-23 17:16:30 +0800

    Secondary Menu Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,81 @@
 import React from "react";
 import "../style/Header.css";
+import "../style/Frontpage.css";
 import { Link } from "react-router-dom";
 import SiteLogo from "../resources/dumbRabbltLogo.png";
+import { BiMenu } from "react-icons/bi";
 
-class Header extends React.Component {
-  render() {
-    const HeaderItem = ["headerItem", "headerItem", "headerItem", "headerItem"];
-    const HeaderItemContent = [
-      "headerItemContent",
-      "headerItemContent",
-      "headerItemContent",
-      "headerItemContent",
-    ];
-    const routerPath = window.location.pathname;
-    switch (routerPath) {
-      case "/":
-        HeaderItem[0] += " Selected";
-        break;
-      case "/Archive":
-        HeaderItem[2] += " Selected";
-        break;
-      default:
-        break;
-    }
-    return (
-      <React.Fragment>
-        <div className="siteHeader">
-          <img className="logo" src={SiteLogo} alt=""></img>
-          <h1 className="headerSiteName">荐食</h1>
-          {/* 1000px width */}
-          <div className="headerItemSection">
-            <Link to={`/`}>
-              <div className={HeaderItem[0]}>
-                <p className={HeaderItemContent[0]}>HOME</p>
-              </div>
-            </Link>
-            <div className={HeaderItem[1]}>
-              <p className={HeaderItemContent[1]}>EVENTS</p>
-            </div>
-            <Link to={`/Archive`}>
-              <div className={HeaderItem[2]}>
-                <p className={HeaderItemContent[2]}>ARCHIVE</p>
-              </div>
-            </Link>
-            <div className={HeaderItem[3]}>
-              <p className={HeaderItemContent[3]}>MISC</p>
-            </div>
-          </div>
-          <Link to={`/menu`}>
-          <div className="MobileMenu">
-            <p className="MobileMenuContent">≡</p>
-          </div>
-          </Link>
-        </div>
-        
-      </React.Fragment>
-    );
+function Header() {
+  const HeaderItem = ["headerItem", "headerItem", "headerItem", "headerItem"];
+  const HeaderItemContent = [
+    "headerItemContent",
+    "headerItemContent",
+    "headerItemContent",
+    "headerItemContent",
+  ];
+  const routerPath = window.location.pathname;
+  switch (routerPath) {
+    case "/":
+      HeaderItem[0] += " Selected";
+      break;
+    case "/Archive":
+      HeaderItem[2] += " Selected";
+      break;
+    default:
+      break;
   }
+  return (
+    <React.Fragment>
+      <div className="siteHeader">
+        <img className="logo" src={SiteLogo} alt=""></img>
+        <h1 className="headerSiteName">荐食</h1>
+        {/* 1000px width */}
+        <div className="headerItemSection">
+          <Link to={`/`}>
+            <div className={HeaderItem[0]}>
+              <p className={HeaderItemContent[0]}>HOME</p>
+            </div>
+          </Link>
+          <div className={HeaderItem[1]}>
+            <p className={HeaderItemContent[1]}>EVENTS</p>
+          </div>
+          <Link to={`/Archive`}>
+            <div className={HeaderItem[2]}>
+              <p className={HeaderItemContent[2]}>ARCHIVE</p>
+            </div>
+          </Link>
+          <div className={HeaderItem[3]}>
+            <p className={HeaderItemContent[3]}>MISC</p>
+          </div>
+        </div>
+        <div className="MobileMenuContent">
+          <BiMenu id="ToggleMenu" />
+          <p className="menuItemSection">
+            <Link to={`/`} className="LineLink">
+              <div className="MenuItem">
+                <p className="MenuItemContent">HOME</p>
+              </div>
+            </Link>
+            <Link className="LineLink">
+              <div className="MenuItem">
+                <p className="MenuItemContent">EVENTS</p>
+              </div>
+            </Link>
+            <Link to={`/Archive`} className="LineLink">
+              <div className="MenuItem">
+                <p className="MenuItemContent">ARCHIVE</p>
+              </div>
+            </Link>
+            <Link className="LineLink">
+              <div className="MenuItem">
+                <p className="MenuItemContent">MISC</p>
+              </div>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Header;
--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -2,31 +2,29 @@ import React from "react";
 import "../style/Frontpage.css";
 import { Link } from "react-router-dom";
 
-class MobileMenu extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-          <div className="menuItemSection">
-            <Link to={`/`}>
-              <div className="MenuItem">
-                <p className="MenuItemContent">HOME</p>
-              </div>
-            </Link>
-            <div className="MenuItem">
-              <p className="MenuItemContent">EVENTS</p>
-            </div>
-            <Link to={`/Archive`}>
-              <div className="MenuItem">
-                <p className="MenuItemContent">ARCHIVE</p>
-              </div>
-            </Link>
-            <div className="MenuItem">
-              <p className="MenuItemContent">MISC</p>
-            </div>
+function MobileMenu() {
+  return (
+    <React.Fragment>
+      <div className="menuItemSection">
+        <Link to={`/`}>
+          <div className="MenuItem">
+            <p className="MenuItemContent">HOME</p>
           </div>
-      </React.Fragment>
-    );
-  }
+        </Link>
+        <div className="MenuItem">
+          <p className="MenuItemContent">EVENTS</p>
+        </div>
+        <Link to={`/Archive`}>
+          <div className="MenuItem">
+            <p className="MenuItemContent">ARCHIVE</p>
+          </div>
+        </Link>
+        <div className="MenuItem">
+          <p className="MenuItemContent">MISC</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default MobileMenu;
